275c851132b11af42a07f85f0aaa5abb2355971c
Kane <mamad2k18@gmail.com>
2023-03-02 13:06:25 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const addToDo = document.querySelector(".btn");
+const toDoContainer = document.querySelector(".toDoContainer");
+const inputField = document.querySelector(".inputField");
+
+addToDo.addEventListener("click", function() {
+    return alert("vous avez cliquer sur le bouton");
+    // const ToDo = input.value;
+});
